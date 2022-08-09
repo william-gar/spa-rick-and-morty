e5a6b124c767443f89a101bdb6bb75e02dc70b57
William Garcia <william_garcia_m@outlook.com>
2022-08-08 22:00:49 -0500

    uploading-initial-changes

---
+++ api/src/models/Character.js
@@ -0,0 +1,12 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("character", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+    },
+  });
+};
---
+++ api/src/routes/index.js
@@ -0,0 +1,8 @@
+const { Router } = require("express");
+const { Character } = require("../db");
+
+const router = Router();
+
+// Configurar los routers
+
+module.exports = router;
