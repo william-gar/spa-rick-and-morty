fcc13d14f21ca03069b94f0384a4ce3de3e7c912
William Garcia <william_garcia_m@outlook.com>
2022-08-08 22:46:05 -0500

    model-character-completed

--- api/src/models/Character.js
+++ api/src/models/Character.js
@@ -3,10 +3,46 @@ const { DataTypes } = require("sequelize");
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
-  sequelize.define("character", {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
+  sequelize.define(
+    "character",
+    {
+      id: {
+        type: DataTypes.UUID,
+        default: Datatypes.UUIDV4,
+        primaryKey: true,
+      },
+
+      name: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+
+      species: {
+        type: DataTypes.STRING(25),
+        allowNull: false,
+      },
+
+      origin: {
+        type: DataTypes.STRING(50),
+        allowNull: false,
+      },
+
+      image: {
+        type: Datatypes.STRING,
+      },
+
+      created: {
+        type: DataTypes.STRING,
+      },
+
+      createdInDb: {
+        type: DataTypes.BOOLEAN,
+        defaultValue: true,
+        allowNull: false,
+      },
     },
-  });
+    {
+      timestamps: false,
+    }
+  );
 };
