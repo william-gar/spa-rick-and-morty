53132df536fe00072cc7555a81ad4bb17088f022
William Garcia <william_garcia_m@outlook.com>
2022-08-09 17:54:36 -0500

    initialization-backend

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,8 +1,13 @@
+const { default: axios } = require("axios");
 const { Router } = require("express");
 const { Character } = require("../db");
 
 const router = Router();
 
 // Configurar los routers
+
+router.get("/characters", async (req, res) => {
+  const api = await axios.get("https://rickandmortyapi.com/api/character");
+});
 
 module.exports = router;
