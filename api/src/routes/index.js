4e340228b1e633c248ef85fed5ff30d0b1db717d
William Garcia <william_garcia_m@outlook.com>
2022-08-09 19:34:50 -0500

    route-get-characters

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,4 @@
-const { default: axios } = require("axios");
+const axios = require("axios");
 const { Router } = require("express");
 const { Character } = require("../db");
 
@@ -7,7 +7,45 @@ const router = Router();
 // Configurar los routers
 
 router.get("/characters", async (req, res) => {
-  const api = await axios.get("https://rickandmortyapi.com/api/character");
+  const quantity = 30;
+  let characters = [];
+
+  let api = await axios.get("https://rickandmortyapi.com/api/character");
+
+  characters = [...api.data.results];
+
+  if (quantity > 20) {
+    let apiAux = api;
+    let cantidad = quantity % characters.length; //3
+
+    if (cantidad === 0) cantidad = undefined;
+
+    for (let i = 0; characters.length < quantity; i++) {
+      let api2;
+      api2 = await axios.get(apiAux.data.info.next);
+      apiAux = api2;
+      if (quantity - characters.length < 20) {
+        characters = [...characters, ...api2.data.results.slice(0, cantidad)];
+        break;
+      }
+      characters = [...characters, ...api2.data.results];
+    }
+  }
+
+  characters = characters.filter((char) => {
+    let obj = {
+      id: char.id,
+      name: char.name,
+      species: char.species,
+      origin: char.origin.name,
+      image: char.image,
+      created: char.created,
+    };
+
+    return obj;
+  });
+
+  return res.send(characters);
 });
 
 module.exports = router;
