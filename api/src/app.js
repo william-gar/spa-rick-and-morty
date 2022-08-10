1b95575f3b5077b653dfa561063ae71924cd2ed8
William Garcia <william_garcia_m@outlook.com>
2022-08-09 21:25:46 -0500

    characters-routes-name-changed

--- api/src/app.js
+++ api/src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
-const characterRoutes = require("./routes/characters.routes.js");
+const charactersRoutes = require("./routes/characters.routes.js");
 
 require("./db.js");
 
@@ -25,7 +25,7 @@ server.use((req, res, next) => {
   next();
 });
 
-server.use("/", characterRoutes);
+server.use("/", charactersRoutes);
 
 // Error catching endware.
 server.use((err, req, res, next) => {
