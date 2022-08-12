df9dccc4f262bf469ebeb7544e72cc783b88d734
William Garcia <william_garcia_m@outlook.com>
2022-08-11 20:54:00 -0500

    episodes-in-database-before-start-server

--- api/index.js
+++ api/index.js
@@ -1,12 +1,15 @@
 const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
+const { loadEpisodes } = require("./src/controllers/episodes.controllers");
+const axios = require("axios");
 
 // Syncing all the models at once.
 
 // Para la precarga cuando se levanta el server, ejecutar la funcion getEpisodes(). Al ser una peticion vamos a usar async await.
 
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: true }).then(async () => {
   // getEpisodes();
+  await loadEpisodes(51, true);
   server.listen(3001, () => {
     console.log("Listening at 3001"); // eslint-disable-line no-console
   });
